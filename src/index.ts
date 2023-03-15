4d80a561dd44f6ef452789093d66516b3cb86ca5
Michael Riehemann <michael.riehemann@coremedia.com>
2023-03-15 13:36:29 +0100

    add github action

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 // see https://prettier.io/docs/en/options.html#print-width
 // "Setting max_line_length in an .editorconfig file will configure Prettier’s print width, unless overridden."
 
-
 // the following examples should not be changed, because of "max_line_length = 200" in .editorConfig
-// doesn't work on Windows and Linux.
+// doesn't work on Windows and Linux, but on Mac.
 
 // example 1:
 const testLongVariable = "this is a long string and could be splitted but shouldn't";
