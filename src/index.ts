a2550358a5428528497d923db271abe4c47372ac
Michael Riehemann <michael.riehemann@coremedia.com>
2023-03-15 11:38:27 +0100

    more details

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-const testLongVariable =
-  "this is a long string and could be splitted but shouldn't";
+// see https://prettier.io/docs/en/options.html#print-width
+// "Setting max_line_length in an .editorconfig file will configure Prettier’s print width, unless overridden."
 
-// should not be changed, because of "max_line_length = 200" in .editorConfig
+
+// the following examples should not be changed, because of "max_line_length = 200" in .editorConfig
+// doesn't work on Windows and Linux.
+
+// example 1:
+const testLongVariable = "this is a long string and could be splitted but shouldn't";
+
+// example 2:
 const longArrowFunction = (parameterA: string, parameterB: boolean, parameterC: string) => {
   //nothing
 };
